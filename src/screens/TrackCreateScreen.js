65fe1ab02ad062b40e3f81cf4e0bac92e794c3cb
Nick Tillinghast <nickt2@cox.net>
2020-07-07 22:29:35 -0700

    end of day july 6th

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
@@ -7,21 +7,27 @@ import { requestPermissionsAsync } from 'expo-location';
 
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
 
-    // const startWatching = async () => {
-    //     try {
-    //       const { granted } = await requestPermissionsAsync();
-    //       if (!granted) {
-    //         throw new Error('Location permission not granted');
-    //       }
-    //     } catch (e) {
-    //       setErr(e);
-    //     }
-    //   };
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestPermissionsAsync();
+            if (!granted) {
+                throw new Error('Location permission not granted');
+            }
+        } catch (e) {
+            setErr(e);
+        }
+    };
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
         <SafeAreaView forceInset={{ top: 'always' }} >
             <Text h2>Create a Track</Text>
             <Map />
+            {err ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
     )
 }
