79d777e9f02d669cad9d3f1f5ae6b3676602a0f2
Nick Tillinghast <nickt2@cox.net>
2020-11-10 23:06:18 -0700

    test

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -9,12 +9,12 @@ import { FontAwesome } from '@expo/vector-icons';
 const AccountScreen = () => {
     const { signout } = useContext(AuthContext);
     return (
-        <SafeAreaView forceInset={{ top: 'always' }} >
-            <Text style={{ fontSize: 48 }}>AccountScreen</Text>
-            <Spacer>
-                <Button title="Sign Out" onPress={signout} />
-            </Spacer>
-        </SafeAreaView>
+        // <SafeAreaView forceInset={{ top: 'always' }} >
+        //     <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+        //     <Spacer>
+        //         <Button title="Sign Out" onPress={signout} />
+        //     </Spacer>
+        // </SafeAreaView>
     )
 }
 AccountScreen.navigationOptions = {
